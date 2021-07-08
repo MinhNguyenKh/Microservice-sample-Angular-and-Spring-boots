48eec92592566e4091518a73907d5d0f57b6a05d
Minh Nguyen <minhnk@hikesiya.co.jp>
2021-07-08 12:04:52 +0900

    Update docker-compose for backend services

--- batch/src/main/java/com/minhnk/batch/service/BatchService.java
+++ batch/src/main/java/com/minhnk/batch/service/BatchService.java
@@ -20,7 +20,7 @@ public class BatchService {
     public List<ResponseTemplateVO> getAllPostWithComments() {
 
         List<ResponseTemplateVO> voList = new ArrayList<ResponseTemplateVO>();
-        ResponseEntity<ResponseTemplateVO[]> responseEntity = restTemplate.getForEntity("http://localhost:5000/api/query/posts/P", ResponseTemplateVO[].class);
+        ResponseEntity<ResponseTemplateVO[]> responseEntity = restTemplate.getForEntity("http://api-gateway:5000/api/query/posts/P", ResponseTemplateVO[].class);
         ResponseTemplateVO[] responseTemplateVO = responseEntity.getBody();
 
         voList = Arrays.asList(responseTemplateVO);
@@ -28,7 +28,7 @@ public class BatchService {
         headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
         HttpEntity<List<ResponseTemplateVO>> entity = new HttpEntity<List<ResponseTemplateVO>>(voList, headers);
 
-        String result = restTemplate.exchange("http://localhost:5000/api/query/updatePosts", HttpMethod.PUT, entity, String.class).getBody();
+        String result = restTemplate.exchange("http://api-gateway:5000/api/query/updatePosts", HttpMethod.PUT, entity, String.class).getBody();
 
         return voList;
     }
